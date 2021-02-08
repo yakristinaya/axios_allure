f472c018237412c034b3669ea4c2ed505665fbd6
Kristina Chervyakova <yakristinacher@yandex.ru>
2021-02-09 00:27:15 +0300

    add axios_allure

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const { container } = require('codeceptjs')
+const axios = require('codeceptjs/node_modules/axios').default
+
+axios.interceptors.request.use(function (config) {
+  const allure = container.plugins('allure')
+  allure.addAttachment(`${config.method} ${config.url}`, JSON.stringify(config.data ?? {}), 'application/json')
+  return config
+}, function (error) {
+  const allure = container.plugins('allure')
+  const config = error.config
+  allure.addAttachment(`${config.method} ${config.url}`, JSON.stringify(config.data ?? {}), 'application/json')
+  return Promise.reject(error)
+})
+
+axios.interceptors.response.use(function (response) {
+  const allure = container.plugins('allure')
+  allure.addAttachment('response', JSON.stringify(response.data ?? {}), 'application/json')
+
+  return response
+}, function (error) {
+  const allure = container.plugins('allure')
+  allure.addAttachment(`response`, error.response ? JSON.stringify(error.response.data ?? {}) : error.toString(), 'application/json')
+  return Promise.reject(error)
+})
+
+module.exports = function () {
+
+}
